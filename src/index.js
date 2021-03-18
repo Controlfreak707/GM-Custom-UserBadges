bd541558c77276d4b64300ca692672542cd69ee6
Controlfreak707 <controlfreak707@gmail.com>
2021-03-18 11:59:46 -0700

    ...Fix another incorrect import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import showToast from "@goosemod/toast";
 import { debug } from "@goosemod/logger";
-import settingsPage from "@goosemod/settings";
+import * as settingsPage from "@goosemod/settings";
 
 let settings = {
   null: true,
